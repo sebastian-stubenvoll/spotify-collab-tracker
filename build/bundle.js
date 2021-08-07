e5c3c1eaad0d02e7c4eb2414e66b4a054bc1d99f
sebastian-stubenvoll <49613760+sebastian-stubenvoll@users.noreply.github.com>
2021-08-07 20:25:19 +0000

    Update gh-pages to output generated at 7964a25

--- build/bundle.js
+++ build/bundle.js
@@ -75,22 +75,30 @@
  * Author: Ujjwal Gupta
  *
  * License:
- * const package = require('./package.json');
- * var today = new Date();
- * var dd = today.getDate();
- * var mm = today.getMonth() + 1; //January is 0!
- * var yyyy = today.getFullYear();
- * if (dd < 10) {
- * dd = '0' + dd;
- * }
- * if (mm < 10) {
- * mm = '0' + mm;
- * }
- * var today = dd + '/' + mm + '/' + yyyy;
+ * The MIT License (MIT)
  *
- * exports.banner = `@license :${package.name} - V${package.version} - ${today}
- * https://github.com/ujjwalguptaofficial/JsStore
- * Copyright (c) ${yyyy} @Ujjwal Gupta; Licensed ${package.license}`;
+ * Copyright (c) Ujjwal Gupta
+ *
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
  * 
  *
  * date-fns
